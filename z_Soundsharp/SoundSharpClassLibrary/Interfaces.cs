07b09a225151762eb04675ade83f697125b7053e
[Cedric] <[0320987@student.rocvantwente.nl]>
2021-04-12 14:10:15 +0200

    Soundsharp project t/m opdracht 5

---
+++ z_Soundsharp/SoundSharpClassLibrary/Interfaces.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AudioDevices.Interfaces
+{
+    public class Interfaces
+    {
+        public interface IDisplay 
+        {
+            string GetResolutionInfo();
+            int DisplayWidth { get; set; }
+            int DisplayHeight { get; set; }
+            int TotalPixels { get; }
+        }
+    }
+}
